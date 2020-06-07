f477e64eac4a7522d119d3c898b6c0dc18c361ca
Kazuma Adachi <Kazuma Adachi>
2020-06-07 15:22:58 +0900

    Initial Commit

---
+++ MemoPad/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemoPad
+//
+//  Created by Kazuma Adachi on 2020/06/07.
+//  Copyright © 2020 Kazuma Adachi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
